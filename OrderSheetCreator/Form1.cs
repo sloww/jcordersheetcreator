411a2bdff2aaf16126675374f902463fd0a4e30d
tw0r2 <hust.sh@hotmail.com>
2015-07-20 05:52:54 +0800

    导入客户类别和 客户基本完成
    导入客户类别和 客户基本完成

--- OrderSheetCreator/Form1.cs
+++ OrderSheetCreator/Form1.cs
@@ -82,8 +82,6 @@ namespace OrderSheetCreator
                         if (irow == null) continue;
                         if (irow.Cells.Count < 4) continue;
 
-                        //int maxno = irow.Cells.Count;
-
                         string mysmc = irow.GetCell(11, MissingCellPolicy.RETURN_NULL_AND_BLANK).StringCellValue.Trim();
                         if (mysmc.Length < 2) continue;
                         try
@@ -98,10 +96,15 @@ namespace OrderSheetCreator
                         }
 
                         string gcmc = irow.GetCell(12, MissingCellPolicy.RETURN_NULL_AND_BLANK).StringCellValue.Trim();
+                        string gcmczip = stringZip(gcmc);
                         if (gcmc.Length < 2) continue;
                         if (gongchang.ContainsKey(gcmc) == false)
                         {
                             gongchang.Add(gcmc, mysmc);
+                        }
+                        if (gongchang.ContainsKey(gcmczip) == false)
+                        {
+                            gongchang.Add(gcmczip, mysmc);
                         }
 
                     }
@@ -140,7 +143,29 @@ namespace OrderSheetCreator
                 foreach (var cainzCus in cainzCustomers)
                 {
                     string gcmc = cainzCus.CustomerName.Replace('C', ' ').Trim();
+
+                    string mysmc = findMys(gcmc, gongchang);
+                    if (mysmc != "")
+                    {
+
+                        var _maoyishang = ctx.CusCategory.SingleOrDefault(item => item.CateName == mysmc);
+                        if (_maoyishang != null)
+                        {
+                            cainzCus.SecondNum = _maoyishang.SnNum;
+                            ctlistforupdate.Add(cainzCus);
+                        }
+
+                    }
+                    else
+                    {
+                        using (StreamWriter w = new StreamWriter("log2.txt", true))
+                        {
+                            w.WriteLine(gcmc + "," + stringZip(gcmc));
+                        }
+                    }
+
                     //如果在工厂数据和工厂本地字典中同时有工厂的信息，则试图查询他的贸易商字段
+                    /*
                     if (gongchang.ContainsKey(gcmc))
                     {
                         try
@@ -148,12 +173,8 @@ namespace OrderSheetCreator
                             string mysmc = gongchang[gcmc];
                             var _maoyishang = ctx.CusCategory.SingleOrDefault(item => item.CateName == mysmc);
 
-                            //var _maoyishang = (from item in ctx.CusCategory
-                            //             where item.CateName == mysmc
-                            //             select item).ToList();
-                            //if (_maoyishang != null && _maoyishang.Count == 1)
                             //存在所属贸易商
-                            if(_maoyishang !=null)
+                            if (_maoyishang != null)
                             {
                                 cainzCus.SecondNum = _maoyishang.SnNum;
                                 ctlistforupdate.Add(cainzCus);
@@ -161,7 +182,7 @@ namespace OrderSheetCreator
                             //不存在贸易商
                             else
                             {
-                                using (StreamWriter w = new StreamWriter("log.txt", true))
+                                using (StreamWriter w = new StreamWriter("log1.txt", true))
                                 {
                                     w.WriteLine(gcmc);
                                 }
@@ -173,7 +194,15 @@ namespace OrderSheetCreator
                             MessageBox.Show(ee.Message);
                         }
                     }
+                    else
+                    {
+                        using (StreamWriter w = new StreamWriter("log2.txt", true))
+                        {
+                            w.WriteLine(gcmc);
+                        }
+                    }*/
                 }
+                     
 
 
                 ctlist = cainzCustomers.ToList();
@@ -192,14 +221,51 @@ namespace OrderSheetCreator
                     ctx.SaveChanges();
 
                 }
-
             }
+
+ 
+
 
         }
 
 
         private void Form1_Load(object sender, EventArgs e)
         {
+        }
+
+
+        private bool nameEqual(string s1, string s2)
+        {
+            s1 = stringZip(s1);
+            s2 = stringZip(s2);
+
+
+            return s1.Contains(s2) || s2.Contains(s1);
+        }
+
+        private string stringZip(string s1)
+        {
+            string[] removeStrings = { "有限", "公司", "包装", "材料", "上海", "文教", "礼品", "金属", "制品", "南通", "金属", "体育", "用品", "毛绒", "制造", "不锈钢", "山东", "经贸", "防滑垫", "金属", "中日", "合资", "自行车", "机电", "进出口", "（河源）", "（河源）", "（青岛）", "市", "厂", "工业", "橡胶", "技术开发区", "皮件", "劳保", "国际", "股份", "集团", "贸易", "贸易", "旅游", "汽车", "（深圳）", "家饰", "家居", "食品", "县", "省", "科技", "电子", "针织", "服饰", "炭业", "浙江", "广州", "福州", "泰州", "海陵区", "塑业", "上虞", "海陵区", "昆山", "皓源", "宁波" , "经济", "昭源"};
+            //   
+            foreach (var item in removeStrings)
+            {
+                s1 = s1.Replace(item, "");
+            }
+            return s1.Replace(" ", "");
+        }
+
+        //查找一个公司名称是否近似有对应的贸易公司名称
+
+        private string findMys(string s1,Dictionary<string,string> d1)
+        {
+            if (d1.ContainsKey(s1)) return d1[s1];
+            string s1zip = stringZip(s1);
+            if (d1.ContainsKey(s1zip)) return d1[s1zip];
+            foreach (var item in d1)
+            {
+                if (nameEqual( stringZip(item.Key), s1zip)) return item.Value;
+            }
+            return "";
         }
 
     }
